a7ac114f69b7a4f6e134f49e2105eba476d3644c
seeharrr <142153282+seeharrr@users.noreply.github.com>
2023-09-09 15:58:21 +0500

    Add files via upload

---
+++ datatype.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int marks = 10;
+char alphabet = 'm';
+float average = 5.6;
+
+cout<<" marks ="<<10<<endl;
+cout<<" alphabet ="<<'m'<<endl;
+cout<<" average ="<<5.6<<endl;
+cout<<endl;	
+		
+return 0;		
+}
