cc5cfffce1540adbb9beae8047b305b767c6d47a
seeharrr <142153282+seeharrr@users.noreply.github.com>
2023-09-09 16:55:18 +0500

    Add files via upload

---
+++ size of datatype.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ // find the size of datatype
+ 
+ int size , bytes ;
+ cout<<"the size of int ="<< sizeof (int) <<bytes <<endl;
+ cout<<"the size of bool ="<< sizeof (bool) <<bytes <<endl;
+ cout<<"the size of char ="<< sizeof ( char) <<bytes <<endl;
+ cout<<"the size of float ="<< sizeof ( float) <<bytes <<endl;
+ 		
+	return 0 ;
+}
